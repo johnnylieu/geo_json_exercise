d3006ab19fa8d9d5b371fbd79e8479e4fe48d028
Johnny <JohnnyLieu@yahoo.com>
2022-04-03 19:02:11 -0700

    works on a practice method, still trying to figure out how to implement unittest for this exercise

--- test.py
+++ test.py
@@ -5,5 +5,12 @@ class TestGetFeature(unittest.TestCase):
     def get_feature_test(self):
         self.assertEqual(GeojsonFeatureCollection.get_feature(), int)
 
+# def add(num):
+#     return num + 2
+
+# class TestAdd(unittest.TestCase):
+#     def test_add(self):
+#         self.assertEqual(add(2), 5)
+
 if __name__ == "__main__":
     unittest.main()
